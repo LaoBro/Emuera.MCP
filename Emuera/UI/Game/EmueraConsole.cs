7003918afc1b8b8d9f34ae6c5b91f866f4a6bc1e
Enter <deletedenter@gmail.com>
2026-05-15 21:06:17 +0900

    feat:デバッグモードではエラー時にウィンドウを閉じないように（CRER氏のパッチ）

--- Emuera/UI/Game/EmueraConsole.cs
+++ Emuera/UI/Game/EmueraConsole.cs
@@ -1216,6 +1216,8 @@ internal sealed partial class EmueraConsole : IDisposable
 		}
 		else if (state == ConsoleState.Error)
 		{
+			if (Program.DebugMode)
+               return;
 			if (input == ErrorButtonsText && selectingButton != null && selectingButton.ErrPos != null)
 			{
 				OpenErrorFile(selectingButton.ErrPos);
